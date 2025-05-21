e34c4983e3330222900d5d5b368690df5377245f
Jakub Stec <stecu03@gmail.com>
2025-05-21 20:20:27 +0200

    .

--- NodeBackend/index.js
+++ NodeBackend/index.js
@@ -2,15 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import aiRoutes from './Routing/AiRequest.js';
+
+dotenv.config();
+const app = express();
 app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-dotenv.config();
-const app = express();
-
-app.use(cors());
 app.use(express.json());
 app.use('/ai', aiRoutes);
 
